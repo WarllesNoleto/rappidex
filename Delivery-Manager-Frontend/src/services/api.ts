9daf005c3baf372058b56655f60fd22c4a8ac0d3
Warlles Noleto <warllesb@gmail.com>
2026-03-13 10:00:49 -0300

    Update api.ts

--- Delivery-Manager-Frontend/src/services/api.ts
+++ Delivery-Manager-Frontend/src/services/api.ts
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
-// const apiUrl = 'http://localhost:3000/api'
-const apiUrl = 'https://rappidex-api-eef82025324b.herokuapp.com/api'
+// produção
+//const apiUrl = 'https://rappidex-api-eef82025324b.herokuapp.com/api';
+
+// local (descomente para testes)
+ const apiUrl = 'http://localhost:3000/api';
 
 const api = axios.create({
-    
-    baseURL: apiUrl,
+  baseURL: apiUrl,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
